cd968d2dca29cf73b06fa215f00ff1e791f55342
Lucas Oliveira <lucazolvr@gmail.com>
2024-08-07 11:43:00 -0300

    segundo teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("PRA CIMA LAGO VERDE");
+        System.out.println("PRA CIMA LAGO VERDE quero ver você vencer!");
     }
 }
